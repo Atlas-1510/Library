58ee55e5f91803b69d09e94aa3a8c4a25f335ad9
Jason Aravanis <j.aravanis@icloud.com>
2020-11-01 17:25:06 +1100

    fixing read/unread toggle

--- library.js
+++ library.js
@@ -1,5 +1,18 @@
+// Array to store book objects in the library
 const myLibrary = [];
+// DOM element to display book objects
 const libraryMain = document.getElementById("library");
+// Global counter to give an index number to each book, to link tile DOM element with actual book object
+let bookIndex = 0;
+
+
+// Book Constructor
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
 
 // Dummy books to start with
 let debug = true;
@@ -24,27 +37,8 @@ function addBookFromArray(array) {
     let newAuthor = array[1];
     let newPages = array[2];
     let newRead = array[3];
-    let book = newBook(newTitle, newAuthor, newPages, newRead);
+    let book = new Book(newTitle, newAuthor, newPages, newRead);
     myLibrary.push(book);
-}
-
-// Book Constructor
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-}
-
-// Book prototype behaviours - getInfo()
-Book.prototype.getInfo = function () {
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${(this.read) ? "read." : "not yet read."}`
-}
-
-// Create a book
-function newBook(title, author, pages, read) {
-    let book = new Book(title, author, pages, read);
-    return book;
 }
 
 // Display library on screen
@@ -52,35 +46,29 @@ myLibrary.forEach((book) => {
     addBookToLibrary(book);
 });
 
-function addBookToLibrary(book) {
-    // For each book, make a tile in the library and show the books information within that tile
+
+function newTile(book) {
+    // Tile Header (title and buttons)
+    // (title)
     let newBookTile = document.createElement("article");
     newBookTile.classList.add("tile");
-    // Book title
     let tileHeader = document.createElement("div");
     tileHeader.classList.add("tileHeader");
     let tileTitle = document.createElement("h1");
     tileTitle.textContent = book.title;
     tileHeader.appendChild(tileTitle);
-    // Read/unread toggle
+    // (buttons to read or delete)
+    let buttonHolder = document.createElement("div");
+    buttonHolder.classList.add("buttonHolder");
     let tileReadToggle = document.createElement("button");
-    tileReadToggle.setAttribute("data-read-toggle", book.read);
+    tileReadToggle.setAttribute("data-read-toggle", "");
     tileReadToggle.textContent = (book.read) ? "Unread?" : "Read?";
-    // Delete book button
+    buttonHolder.appendChild(tileReadToggle);
     let tileDeleteButton = document.createElement("button");
     tileDeleteButton.setAttribute("data-delete-book", "");
     tileDeleteButton.innerHTML = "&times;";
-    newBookTile.appendChild(tileHeader)
-
-    // Div to hold buttons
-    let buttonHolder = document.createElement("div");
-    buttonHolder.id = "buttonHolder";
-    buttonHolder.appendChild(tileReadToggle);
     buttonHolder.appendChild(tileDeleteButton);
     tileHeader.appendChild(buttonHolder);
-
-
-
     // Book information
     let tileList = document.createElement("ul");
     let tileAuthor = document.createElement("li");
@@ -89,11 +77,23 @@ function addBookToLibrary(book) {
     tilePages.textContent = `${book.pages} pages`
     let tileRead = document.createElement("li");
     tileRead.textContent = (book.read) ? "Read" : "Not read";
+
     tileList.appendChild(tileAuthor);
     tileList.appendChild(tilePages);
     tileList.appendChild(tileRead);
+    // Add components to tile
+    newBookTile.appendChild(tileHeader)
     newBookTile.appendChild(tileList);
-    libraryMain.appendChild(newBookTile);
+    return newBookTile;
+}
+
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+    // For each book, make a tile in the library and show the books information within that tile
+    let tile = newTile(book);
+    tile.setAttribute("data-book-index", bookIndex);
+    bookIndex++;
+    libraryMain.appendChild(tile);
 }
 
 
@@ -140,14 +140,10 @@ function closeModal(modal) {
 const addBookSubmitButton = document.getElementById("addBookSubmit");
 addBookSubmitButton.addEventListener("click", () => {
     let bookTitle = document.getElementById("bookTitle").value;
-    console.log(bookTitle);
     let bookAuthor = document.getElementById("bookAuthor").value;
-    console.log(bookAuthor);
     let bookPages = document.getElementById("bookPages").value;
-    console.log(bookPages);
     let bookRead = (document.querySelector('input[name="read"]:checked').value == "Read") ? true : false;
-    console.log(bookRead);
-    let addBook = newBook(bookTitle, bookAuthor, bookPages, bookRead);
+    let addBook = new Book(bookTitle, bookAuthor, bookPages, bookRead);
     addBookToLibrary(addBook);
     let modal = addBookSubmitButton.closest(".modal");
     closeModal(modal);
@@ -157,28 +153,60 @@ addBookSubmitButton.addEventListener("click", () => {
 const deleteBookButtons = document.querySelectorAll("[data-delete-book]");
 deleteBookButtons.forEach(button => {
     button.addEventListener("click", () => {
-        let bookTarget = button.closest(".tile");
-        deleteBook(bookTarget);
+        console.log("active");
+        button.closest(".tile").remove();
+        let libraryIndex = button.closest(".tile").getAttribute("data-book-index");
+        myLibrary.splice(libraryIndex, 1);
     })
 });
 
-function deleteBook(bookTile) {
-    bookTile.remove();
-}
 
 // Read/Unread book functionality
 const readToggles = document.querySelectorAll("[data-read-toggle]");
 readToggles.forEach(button => {
+    readButtonToggle(button)
+});
+
+function refreshTile(tile) {
+    console.log("refresh tile activated");
+    // Need position the tile was in, remember so it can be put back in same position
+    let allNodes = Array.from(libraryMain.childNodes);
+    // console.log(`All nodes: ${allNodes}`);
+    // console.log(`Number of nodes: ${allNodes.length}`);
+    let tilePosition = allNodes.indexOf(tile);
+    // console.log(`tile position (counting from zero): ${tilePosition}`);
+    if (tilePosition == allNodes.length - 1) {
+        tilePosition = null;
+        // console.log(`Last tile, tilePosition is ${tilePosition}`);
+    }
+    let bookSourceIndex = tile.getAttribute("data-book-index");
+    // console.log(`bookSourceIndex: ${bookSourceIndex}`);
+
+    libraryMain.removeChild(tile);
+
+    // recreate the tile, and add it back in the position it was in before.
+    let refreshedTile = newTile(myLibrary[bookSourceIndex]);
+    // console.log(`refreshedTile bookSourceIndex: ${bookSourceIndex}`);
+    refreshedTile.setAttribute("data-book-index", bookSourceIndex);
+    libraryMain.insertBefore(refreshedTile, libraryMain.childNodes[tilePosition])
+
+    // Add a new click event listener to the newly created button
+    // let button = refreshedTile.childNodes[0].childNodes[1].childNodes[0];
+    let button = refreshedTile.querySelectorAll(".tileHeader .buttonHolder [data-read-toggle]");
+    console.log(`Printing button....`);
+    console.log(button);
+    readButtonToggle(button);
+}
+
+function readButtonToggle(button) {
+    console.log(`Activated.`)
+    console.log(button);
     button.addEventListener("click", () => {
-        let bookTarget = button.closest(".tile");
-        let isRead = (button.dataset.readToggle == "true");
-        if (isRead) {
-            // When the button is pressed, if unread change to read. and vice versa.
-            bookTarget.children[1].children[2].textContent = "Not read";
-            button.textContent = "Read?"
-        } else {
-            bookTarget.children[1].children[2].textContent = "Read";
-            button.textContent = "Unread?"
-        }
+        console.log("click registered");
+        // Get the book and then run the function on that book
+        let book = myLibrary[button.closest(".tile").getAttribute("data-book-index")];
+        button.textContent = (book.read) ? "Read" : "Unread";
+        book.read = !book.read
+        refreshTile(button.closest(".tile"));
     })
-})
+}
